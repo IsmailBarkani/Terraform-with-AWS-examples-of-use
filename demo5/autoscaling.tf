afed44825ffae769a45a5b2e1801e28a5ecfe053
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-05-21 18:25:28 +0000

    add autoscaling configuration

---
+++ demo5/autoscaling.tf
@@ -0,0 +1,25 @@
+resource "aws_key_pair" "mykey"{
+    key_name="mykey"
+    public_key="${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+resource "aws_launch_configuration" "example-launchconfig"{
+    name_prefix ="example_launchconfig"
+    image_id="${lookup(var.AMIS,var.AWS_REGION)}"
+    instance_type="t2.micro"
+    key_name="${aws_key_pair.mykey.key_name}"
+    vpc_security_groupe_ids=["${aws_security_group.groupe.id}"]
+}
+resource "aws_autoscaling_group" "example-autoscaling"{
+    name ="example-autoscaling"
+    vpc_zone_identifier=["${aws_subnet.main-public-1.id}","${aws_subnet.main-public-2}"] #for the high availibility
+    min_size=1
+    max_size=2
+    health_check_grace_period=300 #in seconds
+    health_check_type ="EC2"
+    force_delete =true
+    tag {
+        key="Name"
+        value="ec2 instance"
+        propagate_at_launch=true
+    }
+}
