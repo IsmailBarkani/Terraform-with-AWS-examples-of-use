cb4e26df29c5bb6b31b3625440154ec5ab52f2e6
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-05-22 01:29:01 +0000

    add elstic load balancer

--- demo6/autoscaling.tf
+++ demo6/autoscaling.tf
@@ -8,15 +8,17 @@ resource "aws_launch_configuration" "example-launchconfig" {
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.mykey.key_name}"
   security_groups = ["${aws_security_group.groupe.id}"]
+  user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'this is: '$MYIP > /var/www/html/index.html"
 }
 resource "aws_autoscaling_group" "example-autoscaling" {
   name                      = "example-autoscaling"
   vpc_zone_identifier       = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"] #for the high availibility
   launch_configuration      = "${aws_launch_configuration.example-launchconfig.name}"
-  min_size                  = 1
+  min_size                  = 2
   max_size                  = 2
   health_check_grace_period = 300 #in seconds
-  health_check_type         = "EC2"
+  health_check_type         = "ELB"
+  load_balancers            = ["${aws_elb.my-elb.name}"]
   force_delete              = true
   tag {
     key                 = "Name"
